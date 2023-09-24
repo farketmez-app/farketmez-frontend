bf254b606d12dce3c29e7d59c262fae99035a466
w1that <marmarisco860@gmail.com>
2023-09-24 17:48:23 +0300

    feat(pages): add pages, implement the WelcomePage

---
+++ src/pages/activities-list-page/ActivitiesListPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./activities-list-page.css";
+
+function ActivitiesListPage({ list }) {
+  // list is either "my-own" or "system", fetch data accordingly
+
+  return <div>ActivitiesListPage</div>;
+}
+
+export default ActivitiesListPage;
---
+++ src/pages/sign-in-page/SignInPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sign-in-page.css";
+
+function SignInPage() {
+  return <div>SignInPage</div>;
+}
+
+export default SignInPage;
