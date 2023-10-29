d441ddb41001f602839e7c640f97a3ea2f118ff2
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2023-10-29 22:59:41 +0300

    eksik kalan sayfaya eklem yapıldı css genişletildi

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './header.css';
 
 function Header() {
+	const navigate = useNavigate();
     return (
         <header className="header">
 			<div className="header_logo">
 			<img src="/favicon.ico" alt="Farketmez Logo" className="header_favicon" />
 			Fark Etmez
 			</div>
-			<button class="header-button">
+			<button className="header-button" onClick={() => navigate("/sign-in")}>
 				<i class="bi bi-door-open"></i> Giriş Yap
 			</button>
         </header>
