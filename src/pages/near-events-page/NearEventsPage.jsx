fadd825cdb0f148c4c646d11faebc1af5c84e61c
w1that <marmarisco860@gmail.com>
2023-10-31 15:24:57 +0300

    refactor(pages): create new pages and their routes

---
+++ src/pages/near-events-page/NearEventsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './near-events-page.css'
+
+function NearEventsPage() {
+  return (
+    <div>NearEventsPage</div>
+  )
+}
+
+export default NearEventsPage
---
+++ src/pages/schedule-event-page/ScheduleEventPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ScheduleEventPage() {
+  return (
+    <div>ScheduleEventPage</div>
+  )
+}
+
+export default ScheduleEventPage
