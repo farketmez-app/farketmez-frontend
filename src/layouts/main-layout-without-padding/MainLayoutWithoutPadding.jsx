c91eb7e279f2b9a8a72357446763139507b108d7
w1that <marmarisco860@gmail.com>
2023-10-31 15:23:20 +0300

    feat(layouts): create main layouts

---
+++ src/layouts/main-layout-without-padding/MainLayoutWithoutPadding.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main-layout-without-padding.css'
+
+function MainLayoutWithoutPadding() {
+  return (
+    <div>MainLayoutWithoutPadding</div>
+  )
+}
+
+export default MainLayoutWithoutPadding
