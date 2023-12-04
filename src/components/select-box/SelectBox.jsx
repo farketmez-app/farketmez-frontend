1e1416e10e833ea3d7a3bb39e4c901d4497b7504
mithat akbulut <w1that@192.168.1.33>
2023-12-04 18:38:45 +0300

    refactor: make SelectBox show image optionally

--- src/components/select-box/SelectBox.jsx
+++ src/components/select-box/SelectBox.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import './select-box.css'
+import "./select-box.css";
 
-export default function SelectBox({ option, toggleSelectBox, image, text, selectedOnes }) {
+export default function SelectBox({
+  option,
+  toggleSelectBox,
+  image,
+  text,
+  selectedOnes,
+  customClassname,
+}) {
   return (
-    <button className={`select-box ${selectedOnes.includes(option) ? 'select-box-selected' : ''}`}
-    
-    onClick={() => toggleSelectBox(option)}>
-      <img src={image} alt={text} />
+    <button
+      className={`select-box ${
+        selectedOnes.includes(option) ? "select-box-selected" : ""
+      } ${customClassname ? customClassname : ""}`}
+      onClick={() => toggleSelectBox(option)}
+    >
+      {image && <img src={image} alt={text} />}
       <p>{text}</p>
     </button>
   );
