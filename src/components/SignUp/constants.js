c1ae5b33ab53121fc5d3b4eace7a7538def00f0f
w1that <marmarisco860@gmail.com>
2023-12-04 13:16:50 +0300

    feat: add interests array as const

---
+++ src/components/SignUp/constants.js
@@ -0,0 +1,78 @@
+import ArtImage from "../../assets/images/interests/art.png";
+import ChildrenImage from "../../assets/images/interests/children.png";
+import CinemaImage from "../../assets/images/interests/cinema.png";
+import FoodImage from "../../assets/images/interests/food.png";
+import GymImage from "../../assets/images/interests/gym.png";
+import MuseumImage from "../../assets/images/interests/museum.png";
+import NatureImage from "../../assets/images/interests/nature.png";
+import NightImage from "../../assets/images/interests/night.png";
+import ReadingImage from "../../assets/images/interests/reading.png";
+import SportsImage from "../../assets/images/interests/sports.png";
+import TheatreImage from "../../assets/images/interests/theatre.png";
+import MusicImage from "../../assets/images/interests/music.png";
+
+const interests = [
+  {
+    name: "Sinema",
+    image: CinemaImage,
+    selected: false,
+  },
+  {
+    name: "Restoran ve yemek",
+    image: FoodImage,
+    selected: false,
+  },
+  {
+    name: "Konserler ve canlı müzik",
+    image: MusicImage,
+    selected: false,
+  },
+  {
+    name: "Tiyatro",
+    image: TheatreImage,
+    selected: false,
+  },
+  {
+    name: "Sanat sergileri ve galeriler",
+    image: ArtImage,
+    selected: false,
+  },
+  {
+    name: "Spor",
+    image: SportsImage,
+    selected: false,
+  },
+  {
+    name: "Doğa yürüyüşleri ve açık hava",
+    image: NatureImage,
+    selected: false,
+  },
+  {
+    name: "Gece kulüpleri ve eğlence mekanları",
+    image: NightImage,
+    selected: false,
+  },
+  {
+    name: "Kitap okuma ve edebiyat",
+    image: ReadingImage,
+    selected: false,
+  },
+  {
+    name: "Müzeler",
+    image: MuseumImage,
+    selected: false,
+  },
+  {
+    name: "Çocuk etkinlikleri",
+    image: ChildrenImage,
+    selected: false,
+  },
+
+  {
+    name: "Yoga ve spor salonları",
+    image: GymImage,
+    selected: false,
+  },
+];
+
+export { interests };
